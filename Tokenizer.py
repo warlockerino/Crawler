181d50195603288d3e1c9fb74bf4fc0a54a41444
warlockerino <laidbacklenz@googlemail.com>
2015-04-29 10:44:44 +0200

    Tokenizer added

---
+++ Tokenizer.py
@@ -0,0 +1,38 @@
+class Tokenizer():
+	def __init__(self):
+		self.tokens = {}
+		self.stopWords = ['d01', 'd02', 'd03', 'd04', 'd05', 'd06', 'd07', 'd08',  
+'a', 'also', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'do',
+'for', 'have', 'is', 'in', 'it', 'of', 'or', 'see', 'so',
+'that', 'the', 'this', 'to', 'we']
+	
+	# ADD URL ELEMENT TO UPDATE ITS TOKENS
+	def addUrl(self, url):
+		self.name 		= url.name
+		self.content 	= url.content
+		self.content 	= self.content.lower()
+		self.content 	= self.content.replace(".", " ")
+		self.content 	= self.content.replace(";", " ")
+		self.content 	= self.content.replace(",", " ")
+		self.content 	= self.content.split()
+
+		tmpTokens = {}
+		if self.name not in self.tokens:
+			for t in self.content:
+				if t not in self.stopWords:
+					if t not in tmpTokens:
+						tmpTokens[t] = 1
+					else:
+						tmpTokens[t] += 1
+
+
+
+			self.tokens[self.name] = tmpTokens
+	
+	def printMap(self):
+		print self.tokens		
+
+
+
+
+
